148719800c3d6df272762ed1bac47b8ddf0f097a
NoneDudeXCX <nuho.hasan10@gmail.com>
2026-01-27 00:23:54 +0300

    Stable being tested

--- downloads/Old version/NotesIDE-Windows/Windows/CLICK_ME.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-where python3 >nul 2>&1
-if %errorlevel%==0 (
-    powershell -Command "Start-Process python3 -ArgumentList 'IGNORE_ME.py' -Verb RunAs"
-) else (
-    powershell -Command "Start-Process python -ArgumentList 'IGNORE_ME.py' -Verb RunAs"
-)
